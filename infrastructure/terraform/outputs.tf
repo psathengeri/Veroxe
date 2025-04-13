87b01aa6da3cfc85ecaf3b6a5c1d0913f8e8310d
Pavan <psatheng@purdue.edu>
2025-04-13 17:38:45 -0400

    Successfully Created GCP resources

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -10,6 +10,6 @@ output "cloud_run_url" {
   value = google_cloud_run_service.veroxe_api.status[0].url
 }
 
-output "vertex_featurestore" {
-  value = google_vertex_ai_featurestore.veroxe_featurestore.name
+output "vertex_featurestore_id" {
+  value = google_vertex_ai_featurestore.veroxe_featurestore.id
 }
