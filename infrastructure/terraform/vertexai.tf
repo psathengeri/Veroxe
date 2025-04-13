e311cbc8fc5c9946c90d93799887c4ca50d0199d
Pavan <psatheng@purdue.edu>
2025-04-13 17:38:45 -0400

    Successfully Created GCP resources

--- infrastructure/terraform/vertexai.tf
+++ infrastructure/terraform/vertexai.tf
@@ -1,4 +1,18 @@
-resource "google_vertex_ai_featurestore" "veroxe_featurestore" {
-  name     = var.vertex_featurestore_name
-  region   = var.region
+resource "random_id" "featurestore_name_suffix" {
+  byte_length = 4
 }
+
+resource "google_vertex_ai_featurestore" "veroxe_featurestore" {
+  name     = "veroxe_fs_${random_id.featurestore_name_suffix.hex}"
+  region   = var.region
+  labels = {
+    environment = "testing"
+  }
+
+  online_serving_config {
+    fixed_node_count = 1
+  }
+
+  force_destroy = true
+}
+
